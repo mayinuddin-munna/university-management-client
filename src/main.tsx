5e0259aeb75cfdca09f377ff7f234475bd068f8e
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-07-05 15:16:13 +0600

    reuseable routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./routes/routes.tsx";
 import { RouterProvider } from "react-router-dom";
-import store from "./redux/features/store.ts";
 import { Provider } from "react-redux";
+import { store } from "./redux/features/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -5,10 +5,10 @@ import CreateStudent from "../pages/admin/CreateStudent";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
+// type TRoute = {
+//   path: string;
+//   element: ReactNode;
+// };
 
 type TSidebarItem = {
   key: string;
@@ -16,7 +16,7 @@ type TSidebarItem = {
   children?: TSidebarItem[];
 };
 
-const adminPaths = [
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -68,21 +68,21 @@ export const adminSidebarItems = adminPaths.reduce(
   []
 );
 
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.element) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       acc.push({
+//         path: child.path,
+//         element: child.element,
+//       });
+//     });
+//   }
 
-  return acc;
-}, []);
+//   return acc;
+// }, []);
