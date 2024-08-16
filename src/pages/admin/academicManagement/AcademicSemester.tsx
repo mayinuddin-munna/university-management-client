1a92270af2a78f2795a83488af0acf4542f74780
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-08-16 19:41:58 +0600

    academic semester refactor

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,4 +1,4 @@
-import { useGetAllSemesterQuery } from "../../../redux/features/auth/academicSemester/academicSemesterApi";
+import { useGetAllSemesterQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
 
 const AcademicSemester = () => {
   const { data } = useGetAllSemesterQuery(undefined);
